f09444f8a9972c50bc95d6152a4aeeda43f2ef5c
hejiawencc <hjw0415@outlook.com>
2026-09-01 16:04:37 +0800

    overlay usbdevice 修复usbdevice启动后自动stop和update时的权限问题, 添加USB_FUNCS=ums时的参数说明

--- overlay/etc/profile.d/usbdevice.sh
+++ overlay/etc/profile.d/usbdevice.sh
@@ -5,9 +5,16 @@
 # option: adb acm hid mtp ntb rndis uac1 uac2 ums uvc
 export USB_FUNCS="rndis"
 
+# UMS configuration when USB_FUNCS=ums
+# UMS_FILE: The image path of USM shared files
 export UMS_FILE=/ums_shared.img
+# UMS_SIZE: The file size automatically created by UMS_FILE
 export UMS_SIZE=256M
+# UMS_FSTYPE: The file system type of UMS_FILE
 export UMS_FSTYPE=vfat
+# UMS_MOUNT: When disconnected, auto mount UMS_FILE to UMS_MOUNTPOINT. 1: enable, 0: disable
 export UMS_MOUNT=0
+# UMS_MOUNTPOINT: The mount point of UMS_FILE when UMS_MOUNT=1
 export UMS_MOUNTPOINT=/mnt/ums
+# UMS_RO: When UMS_MOUNT=1, set UMS_FILE 1:read-only, 0:read-write
 export UMS_RO=0
